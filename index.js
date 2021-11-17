5d2d0cba3dc208f60ca9fda240d7a0ac9572b61f
Vienne Nguen <vienne@labs196.com>
2021-11-17 14:50:40 -0600

    allow CORS

--- index.js
+++ index.js
@@ -10,9 +10,13 @@ mongoose.connect(process.env.DB_CONNECT,{
     useNewUrlParser:true
 },()=>{console.log("Connected to DB")})
 
-
-app.use(express.json());
+app.use(express.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use('/api/user',authRoute)
-
+app.use('/test',()=>{console.log("test")})
 
 app.listen(5000,()=>console.log("Server is running..."))
