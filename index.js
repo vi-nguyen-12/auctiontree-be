28d915049b1ae46995c9dac9aecfe2cbdb58af0e
Vienne Nguen <vienne@labs196.com>
2021-11-27 09:02:05 -0600

    prettier

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-const express=require('express');
-const mongoose=require('mongoose')
-const dotenv=require('dotenv');
-const userRoutes=require("./routes/userRoutes");
-const uploadRoutes=require("./routes/uploadRoutes");
-const cookieparser=require("cookie-parser");
-const cors=require("cors")
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
+const cookieparser = require("cookie-parser");
+const cors = require("cors");
 
-const app =express();
-app.use(cors({credentials:true, origin: "http://localhost:3000"}))
+const app = express();
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieparser());
 
 dotenv.config();
 
-mongoose.connect(process.env.DB_CONNECT,{
-    useNewUrlParser:true
-},()=>{console.log("Connected to DB")})
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
+    useNewUrlParser: true,
+  },
+  () => {
+    console.log("Connected to DB");
+  }
+);
 
-app.use(express.json())
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-app.use('/api/user',userRoutes)
-// app.use("/api/upload", uploadRoutes)
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use("/api/user", userRoutes);
+app.use("/api/upload", uploadRoutes);
 
-app.listen(5000,()=>console.log("Server is running..."))
+app.listen(5000, () => console.log("Server is running..."));
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const router= require('express').Router();
-const uploadController =require("../controller/uploadController")
+const fs = require("fs");
+const router = require("express").Router();
+const uploadController = require("../controller/uploadController");
 
 router.route("/").post(uploadController.uploadFile);
 
-
+module.exports = router;
