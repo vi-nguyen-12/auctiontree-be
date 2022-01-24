f60c9a92eec98c2d79efa16268a7a8534652235d
Vi <vienne@labs196.com>
2022-01-24 17:04:35 -0600

    KYC schema

--- model/Kyc.js
+++ model/Kyc.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
-const kycSchema =  new mongoose.Schema(
-    {
-      userId: { type: String, required: true },
-      kycId: { type: String, unique: true, required: true },    
-      status: { type: String, default: "PENDING" },
-      result: { type: Object },
-      createdAt :  {
-        type: Date, 
-        default: Date.now
-      },
-      updatedAt: {
-        type: Date, 
-        default: Date.now
-      },
-    }
-  );
-  
-module.exports = mongoose.model('Kyc',kycSchema);
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const kycSchema = new mongoose.Schema({
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  kycId: { type: String, unique: true, required: true },
+  status: { type: String, default: "PENDING" },
+  result: { type: Object },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Kyc", kycSchema);
