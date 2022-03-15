84f98ff7c5948a2845642babcfa97165903d51b5
Vi <vienne@labs196.com>
2022-03-15 15:21:21 -0500

    shortened some routes

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -5,16 +5,11 @@ const {
   validateOthers,
 } = require("../middleware/validateRequest");
 const {
-  uploadS3,
-  upload,
-  uploadAll,
   search,
   createRealestate,
   editRealestate,
   getRealEstates,
   getRealEstate,
-  getRealEstatesUpcomingAuctions,
-  getRealEstatesOngoingAuctions,
   getRealEstatesStatusBuyer,
   approveProperty,
   verifyDocument,
@@ -25,49 +20,14 @@ const {
 
 router.get("/real-estates/search", search);
 
-router.post(
-  "/real-estates/images/upload",
-  auth,
-  uploadS3.array("images"),
-  upload
-);
-router.post(
-  "/real-estates/videos/upload",
-  auth,
-  uploadS3.array("videos"),
-  upload
-);
-router.post(
-  "/real-estates/documents/upload",
-  auth,
-  uploadS3.array("documents"),
-  upload
-);
-router.post(
-  "/real-estates/upload",
-  auth,
-  uploadS3.fields([
-    { name: "images" },
-    { name: "videos" },
-    { name: "documents" },
-  ]),
-  uploadAll
-);
-
-router.get("/real-estates/upcomingAuctions", getRealEstatesUpcomingAuctions);
-router.get("/real-estates/ongoingAuctions", getRealEstatesOngoingAuctions);
-
 //this should be only for user is admin
-router.put(
-  "/real-estates/:propertyId/documents/:documentId/status",
-  verifyDocument
-);
-router.put("/real-estates/:propertyId/images/:imageId/status", verifyImage);
-router.put("/real-estates/:propertyId/videos/:videoId/status", verifyVideo);
-router.put("/real-estates/:id/status", approveProperty);
+router.put("/:propertyId/documents/:documentId/status", verifyDocument);
+router.put("/:propertyId/images/:imageId/status", verifyImage);
+router.put("/:propertyId/videos/:videoId/status", verifyVideo);
+router.put("/:id/status", approveProperty);
 
 //for all users
-router.get("/real-estates/:id", getRealEstate);
+router.get("/:id", getRealEstate);
 router.get("/real-estates/", getRealEstates);
 
 //for logged in users
