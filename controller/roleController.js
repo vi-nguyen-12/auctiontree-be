a5bde6bf7ef9340ed740ef5b76e9897463661ed2
Vi <vienne@labs196.com>
2022-08-22 10:43:39 -0500

    api/create role

--- controller/roleController.js
+++ controller/roleController.js
@@ -1,12 +1,6 @@
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
-const Buyer = require("../model/Buyer");
-const User = require("../model/User");
-const Question = require("../model/Question");
-const Property = require("../model/Property");
-const Auction = require("../model/Auction");
-const Docusign = require("../model/Docusign");
-const { sendEmail, replaceEmailTemplate } = require("../helper");
+const Role = require("../model/Role");
 
 //@desc  Create a buyer
 //@route POST /api/buyers body:{auctionId, docusignId,TC, answers:[{questionId, answer: "yes"/"no", explanation:"", documents:[{officialName:..., name:...,url:...}]}] } TC:{time: ISOString format, IPAddress:...}
@@ -27,45 +21,52 @@ const createRole = async (req, res) => {
             "technical",
             "escrow"
           ),
-        permissions: Joi.array().items(
-          Joi.string().valid(
-            "admin_delete",
-            "admin_edit",
-            "admin_create",
-            "admin_read",
-            "auction_delete",
-            "auction_edit",
-            "auction_create",
-            "auction_read",
-            "auction_winner_edit",
-            "auction_winner_read",
-            "property_delete",
-            "property_edit",
-            "property_create",
-            "property_read",
-            "property_img_video_approval",
-            "property_document_approval",
-            "property_approval",
-            "buyer_delete",
-            "buyer_edit",
-            "buyer_create",
-            "buyer_read",
-            "buyer_document_approval",
-            "buyer_answer_approval",
-            "buyer_approval"
+        permissions: Joi.array()
+          .items(
+            Joi.string().valid(
+              "admin_delete",
+              "admin_edit",
+              "admin_create",
+              "admin_read",
+              "auction_delete",
+              "auction_edit",
+              "auction_create",
+              "auction_read",
+              "auction_winner_edit",
+              "auction_winner_read",
+              "property_delete",
+              "property_edit",
+              "property_create",
+              "property_read",
+              "property_img_video_approval",
+              "property_document_approval",
+              "property_approval",
+              "buyer_delete",
+              "buyer_edit",
+              "buyer_create",
+              "buyer_read",
+              "buyer_document_approval",
+              "buyer_answer_approval",
+              "buyer_approval"
+            )
           )
-        ),
+          .required(),
       });
       const { error } = bodySchema.validate(req.body);
       if (error)
         return res.status(200).send({ error: error.details[0].message });
 
-      const newRole = await Role.create(req.body);
+      const savedName = name.trim().split(" ").join("_");
+      const newRole = await Role.create({
+        name: savedName,
+        department,
+        permissions,
+      });
       return res.status(200).send(newRole);
     }
-    return res.stpatus(200).send({ error: "Not allowed to create role" });
+    return res.status(200).send({ error: "Not allowed to create role" });
   } catch (err) {
-    res.status(500).send(err);
+    res.status(500).send(err.message);
   }
 };
 
